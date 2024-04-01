481928dc3e4d91788fca87be6a88ebb27a67ea73
Pathi14 <pathipayong@gmail.com>
2024-04-01 20:13:57 +0200

    setup du projet - OK

--- src/infrastructure/database/database.module.ts
+++ src/infrastructure/database/database.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: parseInt(process.env.DATABASE_PORT),
+            database: process.env.DATABASE_NAME,
+            entities: [],
+            synchronize: true,
+        }),
+    ],
+})
 export class DatabaseModule {}
